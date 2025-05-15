885d0e4ff65a2c5f1c2c3129090f1e396ed1df89
Rodrigo <cabralrodrigo787@gmail.com>
2025-05-15 18:10:23 -0400

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 
         LocalDate date = LocalDate.parse("2000-01-25");
         Seller seller = new Seller(1,"Maria","maria@gmail.com", date,3000.0,obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
 
         System.out.println(seller);
     }
