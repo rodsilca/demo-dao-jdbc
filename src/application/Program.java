5c00e76fa7135c9f0ac785a20d94d4cc8759ff4a
Rodrigo <cabralrodrigo787@gmail.com>
2025-05-15 17:48:39 -0400

    Seller Entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
 import model.entities.Department;
+import model.entities.Seller;
+
+import java.time.LocalDate;
 
 public class Program {
     public static void main(String[] args) {
 
         Department obj = new Department(1,"Books");
 
-        System.out.println(obj);
+        LocalDate date = LocalDate.parse("2000-01-25");
+        Seller seller = new Seller(1,"Maria","maria@gmail.com", date,3000.0,obj);
+        
+        System.out.println(seller);
     }
 }
