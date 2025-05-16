1d67e57b9b3ec89f99c4b8bea82e9fb24e37f8a0
Rodrigo <cabralrodrigo787@gmail.com>
2025-05-16 15:45:23 -0400

    Update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -14,7 +14,7 @@ public class Program {
 
         SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println("=== teste 1: sellerFindById====");
-        Seller seller = sellerDao.findfById(3);
+        Seller seller = sellerDao.findById(3);
         System.out.println(seller);
 
         System.out.println("=== teste 2: sellerFindByDepartmentId====");
@@ -30,6 +30,12 @@ public class Program {
         Seller newSeller = new Seller(null,"Greg","greg@gmail.com",new Date(),4000.0,department);
         sellerDao.insert(newSeller);
         System.out.println("Inserted! new Id: "+newSeller.getId());
+
+        System.out.println("=== teste 5: sellerUpdate====");
+        seller = sellerDao.findById(1);
+        seller.setName("Martha");
+        sellerDao.update(seller);
+        System.out.println("Update completed!");
 
     }
 }
